09a0a67ea193eeb149e514be61f6f8b1b173cdf6
josemicode <jmiguelojedagutierrez@gmail.com>
2026-02-17 20:32:27 +0100

    Update filters.py

--- billing/api/filters.py
+++ billing/api/filters.py
@@ -1,11 +1,13 @@
 import django_filters
+
 from ..models import Invoice
 
 
 class InvoiceFilter(django_filters.FilterSet):
     invoice_no = django_filters.CharFilter(lookup_expr="icontains")
     issued_on = django_filters.DateFromToRangeFilter()
+    provider = django_filters.NumberFilter()
 
     class Meta:
         model = Invoice
-        fields = ["invoice_no", "issued_on"]
+        fields = ["invoice_no", "issued_on", "provider"]
